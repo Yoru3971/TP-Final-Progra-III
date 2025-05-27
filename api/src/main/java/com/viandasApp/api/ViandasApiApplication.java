85ca4ed26e1cdc398c6488fb6e10824b300aa6e0
Emiliano Daverio <150267786+Yoru3971@users.noreply.github.com>
2025-05-27 14:42:49 -0300

    Primer commit

---
+++ api/src/main/java/com/viandasApp/api/ViandasApiApplication.java
@@ -0,0 +1,13 @@
+package com.viandasApp.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViandasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViandasApiApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/viandasApp/api/ViandasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viandasApp.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViandasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
