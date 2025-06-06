e3da1481d10c48e7924574e2433a2800677917b3
Tomas-Kauand <kauandtomas@gmail.com>
2025-06-06 20:17:50 -0300

    Creada clase Emprendimiento (model)
    - hay que revisar atributos
    - la validación del rol del usuario va a estar en el DTO (lógica de negocio)

---
+++ api/src/main/java/com/viandasApp/api/Emprendimiento/model/Emprendimiento.java
@@ -0,0 +1,41 @@
+package com.viandasApp.api.Emprendimiento.model;
+
+import com.viandasApp.api.User.model.User;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "emprendimientos")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Emprendimiento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    @NotBlank
+    private String nombreEmprendimiento;
+
+    @Column(nullable = false)
+    @NotBlank
+    private String ciudad;
+
+    @Column(nullable = false)
+    private String direccion;
+
+    @Column(nullable = false)
+
+    private Long telefono;
+
+    @Column(nullable = false)
+    private User usuario;
+    
+
+}
