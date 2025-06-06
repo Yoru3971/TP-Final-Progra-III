3f44d1bc506681661444e0b327339af1a13608f8
Emiliano Daverio <150267786+Yoru3971@users.noreply.github.com>
2025-06-06 15:40:08 -0300

    Creacion Clase Pedido
    Se creo la clase pedido con su respectivo Enum y la clase Intermedia PedidoVianda

---
+++ api/src/main/java/com/viandasApp/api/Pedido/model/Pedido.java
@@ -0,0 +1,36 @@
+package com.viandasApp.api.Pedido.model;
+
+import com.viandasApp.api.User.model.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Pedido {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "cliente_id")
+    private User cliente;
+
+    //@ManyToOne(optional = false)
+    //@JoinColumn(name = "emprendimiento_id")
+    //private Emprendimiento emprendimiento;
+
+    private LocalDateTime fechaPedido;
+
+    @Enumerated(EnumType.STRING)
+    private EstadoPedido estado;
+
+    @OneToMany(mappedBy = "pedido", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<PedidoVianda> items = new ArrayList<>();
+}
