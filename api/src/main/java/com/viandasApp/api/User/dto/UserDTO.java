1af8fb2d33215dcdf19651c61010adfbd5a330c9
Tomas-Kauand <kauandtomas@gmail.com>
2025-06-06 19:37:51 -0300

    Modificación de application.properties
    Para que cada uno pueda usar su usuario y contraseña de MySQL sin tener que estar cambiando los datos con cada push, cada uno tiene que crear un "archivo application-local.properties" en la carpeta resources (en ese archivo ponen user y password). Ese archivo ya está en el gitignore así que sólo lo tienen que crear.

--- api/src/main/java/com/viandasApp/api/User/dto/UserDTO.java
+++ api/src/main/java/com/viandasApp/api/User/dto/UserDTO.java
@@ -32,42 +32,42 @@ public class UserDTO {
     @NotNull(message = "El rol es obligatorio.")
     private UserRole role;
 
-    public UserDTO(Long id, String fullName, String email, UserRole role) {
-        this.id = id;
-        this.fullName = fullName;
-        this.email = email;
-        this.role = role;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public UserRole getRole() {
-        return role;
-    }
-
-    public void setRole(UserRole role) {
-        this.role = role;
-    }
+//    public UserDTO(Long id, String fullName, String email, UserRole role) {
+//        this.id = id;
+//        this.fullName = fullName;
+//        this.email = email;
+//        this.role = role;
+//    }
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public String getFullName() {
+//        return fullName;
+//    }
+//
+//    public void setFullName(String fullName) {
+//        this.fullName = fullName;
+//    }
+//
+//    public String getEmail() {
+//        return email;
+//    }
+//
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+//
+//    public UserRole getRole() {
+//        return role;
+//    }
+//
+//    public void setRole(UserRole role) {
+//        this.role = role;
+//    }
 }
