8ee7a629a648b8fb1ac5e744807af77e8780cb8e
Tomas-Kauand <kauandtomas@gmail.com>
2025-06-07 14:30:39 -0300

    Corregido Repositorio Emprendimiento

--- api/src/main/java/com/viandasApp/api/Emprendimiento/repository/EmprendimientoRepository.java
+++ api/src/main/java/com/viandasApp/api/Emprendimiento/repository/EmprendimientoRepository.java
@@ -1,16 +1,19 @@
 package com.viandasApp.api.Emprendimiento.repository;
 
 import com.viandasApp.api.Emprendimiento.model.Emprendimiento;
+import com.viandasApp.api.User.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface EmprendimientoRepository extends JpaRepository<Emprendimiento, Long> {
 
-    List<Emprendimiento> findByContieneNombreEmprendimiento(String nombreEmprendimiento);
+    List<Emprendimiento> findByNombreEmprendimientoContaining(String nombreEmprendimiento);
     List<Emprendimiento> findByCiudad(String ciudad);
     List<Emprendimiento> findByDireccion(String direccion);
-    //  Revisar si conviene por Id o por usuario
-    List<Emprendimiento> findByDuenioId(String id);
+    List<Emprendimiento> findByUsuario(User usuario);
+    List<Emprendimiento> findByUsuarioId(Long id);
 
 }
