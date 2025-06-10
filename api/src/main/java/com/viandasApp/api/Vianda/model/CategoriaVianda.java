da52393d41d23b2d2cdec8342a4aff81c8c6f683
Rocio Romanazzi <rocioromanazzi@gmail.com>
2025-06-10 17:12:34 -0300

    - Se agregan nuevas categorias de viandas. - Se agregan descripciones amigables con el usuario para cada una de las categorias.

--- api/src/main/java/com/viandasApp/api/Vianda/model/CategoriaVianda.java
+++ api/src/main/java/com/viandasApp/api/Vianda/model/CategoriaVianda.java
@@ -1,9 +1,36 @@
 package com.viandasApp.api.Vianda.model;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum CategoriaVianda {
-    GUISO,
-    CARNE,
-    ENSALADA,
-    MINUTA,
-    POSTRE,
+    MENU_DEL_DIA("Menú del día"),
+    MILANESA("Milanesas"),
+    PASTA("Pastas"),
+    PARRILLA("Parrilla"),
+    WOK("Woks y salteados"),
+    TARTA("Tartas"),
+    EMPANADA("Empanadas"),
+    ENSALADA("Ensaladas"),
+    SOPA("Sopas"),
+    PARA_COMPARTIR("Para compartir"),
+    SUSHI("Sushi"),
+    ARABE("Comida arabe"),
+    JAPONESA("Comida japonesa"),
+    CHINA("Comida china"),
+    PESCADO("Pescados y mariscos"),
+    HAMBURGUESA("Hamburguesas"),
+    POSTRE("Postres"),
+    PIZZA("Pizzas"),
+    SANDWICH("Sandwiches");
+
+    private final String descripcion;
+
+    CategoriaVianda(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    @JsonValue
+    public String getDescripcion() {
+        return descripcion;
+    }
 }
