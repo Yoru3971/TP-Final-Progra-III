72e4e73bb838b4fb5d41890041f446df0537ee74
Rocio Romanazzi <rocioromanazzi@gmail.com>
2025-06-10 17:48:45 -0300

    - Se agregan metodos de busqueda a ViandaRepository

--- api/src/main/java/com/viandasApp/api/Vianda/repository/ViandaRepository.java
+++ api/src/main/java/com/viandasApp/api/Vianda/repository/ViandaRepository.java
@@ -1,8 +1,17 @@
 package com.viandasApp.api.Vianda.repository;
 
+import com.viandasApp.api.Vianda.model.CategoriaVianda;
 import com.viandasApp.api.Vianda.model.Vianda;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ViandaRepository extends JpaRepository<Vianda, Long> {
+import java.util.List;
 
+public interface ViandaRepository extends JpaRepository<Vianda, Long> {
+    List<Vianda> findByCategoria(CategoriaVianda categoriaVianda);
+    List<Vianda> findByNombreViandaContainingIgnoreCase(String nombre);
+    List<Vianda> findByEmprendimientoId(Long id);
+    List<Vianda> findByPrecioBetween(Double min, Double max);
+    List<Vianda> findByEsVeganoTrue();
+    List<Vianda> findByEsVegetarianoTrue();
+    List<Vianda> findByEsSinTaccTrue();
 }
