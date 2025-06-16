b2f75e633a3962b92b3408616e1b3fb7897494ee
Emiliano Daverio <emilianodaverio@gmail.com>
2025-06-16 16:55:24 -0300

    Distincion de roles
    Ahora el sistema distingue el rol de quien esta logueado y le bloquea el acceso a las rutas que no corresponden

--- api/src/main/java/com/viandasApp/api/Usuario/model/Usuario.java
+++ api/src/main/java/com/viandasApp/api/Usuario/model/Usuario.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
 import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
@@ -43,6 +44,8 @@ public class Usuario implements UserDetails {
     @NotNull
     private RolUsuario rolUsuario;
 
+    // === MÉTODOS DE UserDetails ===
+
     @Override
     public String getUsername() {
         return email;
@@ -50,7 +53,7 @@ public class Usuario implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return List.of(new SimpleGrantedAuthority("ROLE_" + rolUsuario.name()));
     }
 
     @Override
