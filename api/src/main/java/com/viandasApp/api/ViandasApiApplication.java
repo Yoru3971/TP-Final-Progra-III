f9cd6f0cc247b30341e660c091d1d5c4892654cf
kevinraipane <162730749+kevinraipane@users.noreply.github.com>
2025-10-26 19:41:58 -0300

    Implementacion .env

--- api/src/main/java/com/viandasApp/api/ViandasApiApplication.java
+++ api/src/main/java/com/viandasApp/api/ViandasApiApplication.java
@@ -1,5 +1,6 @@
 package com.viandasApp.api;
 
+import com.viandasApp.api.Config.DotenvInitializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ViandasApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ViandasApiApplication.class, args);
+		SpringApplication app = new SpringApplication(ViandasApiApplication.class);
+		app.addInitializers(new DotenvInitializer()); // <-- carga variables .env antes de arrancar
+		app.run(args);
 	}
 }
+
+
