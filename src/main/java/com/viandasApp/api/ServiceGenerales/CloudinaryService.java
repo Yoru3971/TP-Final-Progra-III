fd3638589a9492a43cb098832fb0ccc23c5b4a8b
kevinraipane <162730749+kevinraipane@users.noreply.github.com>
2025-12-06 01:40:01 -0300

    Feature - CloudinaryService
    Implementacion de un service general para la gestion de los metodos de Cloudinary

---
+++ src/main/java/com/viandasApp/api/ServiceGenerales/CloudinaryService.java
@@ -0,0 +1,35 @@
+package com.viandasApp.api.ServiceGenerales;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Service
+public class CloudinaryService {
+    private final Cloudinary cloudinary;
+
+    public CloudinaryService(Cloudinary cloudinary) {
+        this.cloudinary = cloudinary;
+    }
+
+    public String subirImagen(MultipartFile file, String carpeta) {
+        try {
+            Map uploadResult = cloudinary.uploader().upload(
+                    file.getBytes(),
+                    ObjectUtils.asMap("folder", carpeta)
+            );
+            return (String) uploadResult.get("secure_url");
+        } catch (IOException e) {
+            throw new ResponseStatusException(
+                    HttpStatus.INTERNAL_SERVER_ERROR,
+                    "Error al subir la imagen a Cloudinary: " + e.getMessage()
+            );
+        }
+    }
+}
