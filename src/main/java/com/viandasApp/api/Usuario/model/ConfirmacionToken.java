61c6a61a3f79782daa5262b6ce0285942060bee2
kevinraipane <162730749+kevinraipane@users.noreply.github.com>
2025-12-26 21:53:49 -0300

    Create ConfirmacionToken.java
    Tabla donde se guardan los token de validacion con fechas de expiracion

---
+++ src/main/java/com/viandasApp/api/Usuario/model/ConfirmacionToken.java
@@ -0,0 +1,40 @@
+package com.viandasApp.api.Usuario.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class ConfirmacionToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String token;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime expiresAt;
+
+    private LocalDateTime confirmedAt;
+
+    @ManyToOne
+    @JoinColumn(nullable = false, name = "usuario_id")
+    private Usuario usuario;
+
+    public ConfirmacionToken(String token, LocalDateTime createdAt, LocalDateTime expiresAt, Usuario usuario) {
+        this.token = token;
+        this.createdAt = createdAt;
+        this.expiresAt = expiresAt;
+        this.usuario = usuario;
+    }
+}
