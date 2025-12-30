5c9588fb9f7f59944f251ad50cbfe00d7dc82c53
Tomas-Kauand <kauandtomas@gmail.com>
2025-12-29 22:42:31 -0300

    Update EmprendimientoRepository
    - se agregaron los métodos correspondientes a la paginación tanto para public/cliente como para dueño

--- src/main/java/com/viandasApp/api/Emprendimiento/repository/EmprendimientoRepository.java
+++ src/main/java/com/viandasApp/api/Emprendimiento/repository/EmprendimientoRepository.java
@@ -1,6 +1,8 @@
 package com.viandasApp.api.Emprendimiento.repository;
 
 import com.viandasApp.api.Emprendimiento.model.Emprendimiento;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -17,4 +19,10 @@ public interface EmprendimientoRepository extends JpaRepository<Emprendimiento, 
     List<Emprendimiento> findByEstaDisponibleTrue();
     List<Emprendimiento> findByUsuarioId(Long id);
     List<Emprendimiento> findByTelefono(String telefono);
+
+    //  Paginación para Public/Cliente
+    Page<Emprendimiento> findByEstaDisponibleTrue(Pageable pageable);
+
+    //  Paginación para Dueño
+    Page<Emprendimiento> findByUsuarioId(Long id, Pageable pageable);
 }
