40c615363cb6a0db66f615f84ef4cf27592034af
kevinraipane <162730749+kevinraipane@users.noreply.github.com>
2026-01-09 00:33:50 -0300

    Update ViandaService.java
    Se agrega metodo para que el admin vea viandas borradas y no borradas

--- src/main/java/com/viandasApp/api/Vianda/service/ViandaService.java
+++ src/main/java/com/viandasApp/api/Vianda/service/ViandaService.java
@@ -1,11 +1,10 @@
 package com.viandasApp.api.Vianda.service;
 
 import com.viandasApp.api.Usuario.model.Usuario;
-import com.viandasApp.api.Vianda.dto.FiltroViandaDTO;
-import com.viandasApp.api.Vianda.dto.ViandaCreateDTO;
-import com.viandasApp.api.Vianda.dto.ViandaDTO;
-import com.viandasApp.api.Vianda.dto.ViandaUpdateDTO;
+import com.viandasApp.api.Vianda.dto.*;
 import com.viandasApp.api.Vianda.model.Vianda;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
@@ -20,6 +19,7 @@ public interface ViandaService {
     List<ViandaDTO> getViandasDisponiblesByEmprendimiento(FiltroViandaDTO filtroViandaDTO, Long idEmprendimiento);
     Optional<ViandaDTO> findViandaById(Long id, Usuario usuario);
     Optional<ViandaDTO> findViandaByIdPublic(Long id);
+    Page<ViandaAdminDTO> getAllViandasForAdmin(Pageable pageable);
 
     //--------------------------Update--------------------------//
     Optional<ViandaDTO> updateVianda(Long id, ViandaUpdateDTO viandaDto, Usuario usuario);
