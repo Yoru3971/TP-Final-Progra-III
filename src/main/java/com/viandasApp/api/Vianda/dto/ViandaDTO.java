d6a24316100df84f3c6a0c6820624e8b7be9e99b
kevinraipane <162730749+kevinraipane@users.noreply.github.com>
2026-01-09 00:32:57 -0300

    Update ViandaDTO.java

--- src/main/java/com/viandasApp/api/Vianda/dto/ViandaDTO.java
+++ src/main/java/com/viandasApp/api/Vianda/dto/ViandaDTO.java
@@ -1,6 +1,5 @@
 package com.viandasApp.api.Vianda.dto;
 
-
 import com.viandasApp.api.Emprendimiento.dto.EmprendimientoDTO;
 import com.viandasApp.api.Vianda.model.CategoriaVianda;
 import com.viandasApp.api.Vianda.model.Vianda;
@@ -13,38 +12,40 @@ import lombok.NoArgsConstructor;
 @Data
 public class ViandaDTO {
     private Long id;
-
     private String nombreVianda;
-
     private CategoriaVianda categoria;
-
     private String descripcion;
-
     private Double precio;
-
     private Boolean esVegano;
-
     private Boolean esVegetariano;
-
     private Boolean esSinTacc;
-
     private EmprendimientoDTO emprendimiento;
-
     private Boolean estaDisponible;
-
     private String imagenUrl;
 
     public ViandaDTO(Vianda vianda) {
         this.id = vianda.getId();
-        this.nombreVianda = vianda.getNombreVianda();
-        this.categoria = vianda.getCategoria();
-        this.descripcion = vianda.getDescripcion();
-        this.precio = vianda.getPrecio();
-        this.esVegano = vianda.getEsVegano();
-        this.esVegetariano = vianda.getEsVegetariano();
-        this.esSinTacc = vianda.getEsSinTacc();
-        this.emprendimiento = new EmprendimientoDTO(vianda.getEmprendimiento());
-        this.estaDisponible = vianda.getEstaDisponible();
         this.imagenUrl = vianda.getImagenUrl();
+        this.emprendimiento = new EmprendimientoDTO(vianda.getEmprendimiento());
+
+        if (vianda.getDeletedAt() != null) {
+            this.nombreVianda = "Vianda Eliminada";
+            this.descripcion = "No disponible";
+            this.categoria = null;
+            this.precio = 0.0;
+            this.esVegano = false;
+            this.esVegetariano = false;
+            this.esSinTacc = false;
+            this.estaDisponible = false;
+        } else {
+            this.nombreVianda = vianda.getNombreVianda();
+            this.categoria = vianda.getCategoria();
+            this.descripcion = vianda.getDescripcion();
+            this.precio = vianda.getPrecio();
+            this.esVegano = vianda.getEsVegano();
+            this.esVegetariano = vianda.getEsVegetariano();
+            this.esSinTacc = vianda.getEsSinTacc();
+            this.estaDisponible = vianda.getEstaDisponible();
+        }
     }
 }
