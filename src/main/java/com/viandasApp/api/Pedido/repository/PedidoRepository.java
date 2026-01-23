d4b7b09752acb102f6b5fa59e011ab38569296e6
Tomas-Kauand <kauandtomas@gmail.com>
2026-01-23 20:25:25 -0300

    Update PedidoRepository
    - se ajustó para permitir el uso de Specification
    - se agregaron consultas personalizadas para obtener los nombres únicos de los emprendimientos (según rol)

--- src/main/java/com/viandasApp/api/Pedido/repository/PedidoRepository.java
+++ src/main/java/com/viandasApp/api/Pedido/repository/PedidoRepository.java
@@ -3,23 +3,30 @@ package com.viandasApp.api.Pedido.repository;
 import com.viandasApp.api.Pedido.model.EstadoPedido;
 import com.viandasApp.api.Pedido.model.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @Repository
-public interface PedidoRepository extends JpaRepository<Pedido, Long> {
-    List<Pedido> findByUsuarioId(Long idCliente);
+public interface PedidoRepository extends JpaRepository<Pedido, Long>, JpaSpecificationExecutor<Pedido> {
+
     List<Pedido> findByEmprendimientoId(Long idEmprendimiento);
-    List<Pedido> findByEmprendimientoIdAndUsuarioId(Long idEmprendimiento, Long idUsuario);
-    List<Pedido> findByEstado(EstadoPedido estado);
-    List<Pedido> findByFechaEntrega(LocalDate fechaEntrega);
-    List<Pedido> findByFechaEntregaAndUsuarioId(LocalDate fechaEntrega, Long idUsuario);
-    List<Pedido> findByFechaEntregaAndEmprendimientoId(LocalDate fechaEntrega, Long idEmprendimiento);
-    //Busca pedidos donde pedido.emprendimiento.usuario.id coincide con el enviado por parametro
-    List<Pedido> findByEmprendimientoUsuarioId(Long idDueno);
     boolean existsByEstadoAndUsuarioId(EstadoPedido estado, Long usuarioId);
     boolean existsByEstadoAndEmprendimientoUsuarioId(EstadoPedido estado, Long duenoId);
     boolean existsByEmprendimientoId(Long idEmprendimiento);
+
+    //  Consultas para obtener los nombres de los emprendimientos distintos (dependiendo de los pedidos)
+
+    @Query("SELECT DISTINCT p.emprendimiento.nombreEmprendimiento FROM Pedido p ORDER BY p.emprendimiento.nombreEmprendimiento")
+    List<String> findDistinctEmprendimientosAdmin();
+
+    @Query("SELECT DISTINCT p.emprendimiento.nombreEmprendimiento FROM Pedido p WHERE p.emprendimiento.usuario.id = :duenoId ORDER BY p.emprendimiento.nombreEmprendimiento")
+    List<String> findDistinctEmprendimientosDueno(Long duenoId);
+
+    @Query("SELECT DISTINCT p.emprendimiento.nombreEmprendimiento FROM Pedido p WHERE p.usuario.id = :clienteId ORDER BY p.emprendimiento.nombreEmprendimiento")
+    List<String> findDistinctEmprendimientosCliente(Long clienteId);
+
 }
