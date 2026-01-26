bc0de38b6b65e41215c510a477cec7dd63d719f3
Tomas-Kauand <kauandtomas@gmail.com>
2026-01-25 23:09:11 -0300

    Update EmprendimientoRepository
    - se adaptó para usar Jpa con Specification

--- src/main/java/com/viandasApp/api/Emprendimiento/repository/EmprendimientoRepository.java
+++ src/main/java/com/viandasApp/api/Emprendimiento/repository/EmprendimientoRepository.java
@@ -4,13 +4,14 @@ import com.viandasApp.api.Emprendimiento.model.Emprendimiento;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface EmprendimientoRepository extends JpaRepository<Emprendimiento, Long> {
+public interface EmprendimientoRepository extends JpaRepository<Emprendimiento, Long>, JpaSpecificationExecutor<Emprendimiento> {
 
     List<Emprendimiento> findByNombreEmprendimientoContaining(String nombreEmprendimiento);
     List<Emprendimiento> findByNombreEmprendimientoContainingAndEstaDisponibleTrue(String nombreEmprendimiento);
