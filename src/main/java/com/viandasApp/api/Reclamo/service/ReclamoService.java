9e3c3b4efb2dc7bbc821a78ef79c7782eb64b38e
Tomas-Kauand <kauandtomas@gmail.com>
2026-01-31 16:09:25 -0300

    Update ReclamoService
    - se agregó el método de búsqueda paginada

--- src/main/java/com/viandasApp/api/Reclamo/service/ReclamoService.java
+++ src/main/java/com/viandasApp/api/Reclamo/service/ReclamoService.java
@@ -3,14 +3,17 @@ package com.viandasApp.api.Reclamo.service;
 import com.viandasApp.api.Reclamo.dto.ReclamoRequestDTO;
 import com.viandasApp.api.Reclamo.model.EstadoReclamo;
 import com.viandasApp.api.Reclamo.model.Reclamo;
+import com.viandasApp.api.Usuario.model.Usuario;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface ReclamoService {
     Reclamo crearReclamo(ReclamoRequestDTO dto);
+    Page<Reclamo> buscarReclamos(Usuario usuario, EstadoReclamo estado, String emailFiltro, Pageable pageable);
     List<Reclamo> listarReclamosPorUsuario(String email);
-    //Para Admin
     List<Reclamo> listarTodosLosReclamos();
     Optional<Reclamo> obtenerReclamoPorId(Long id);
     List<Reclamo> listarReclamosPorEstado(EstadoReclamo estado);
