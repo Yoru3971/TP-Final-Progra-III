7292331fe7dd68a87d8c298dc6830959e00e1132
Tomas-Kauand <kauandtomas@gmail.com>
2026-02-01 13:54:55 -0300

    Fix Endpoint Get Emprendimientos (cliente)
    - se corrigió el error que le mostraba emprendimientos de todas las ciudades al cliente (aunque filtre por ciudad desde el front)

--- src/main/java/com/viandasApp/api/Emprendimiento/controller/EmprendimientoClienteController.java
+++ src/main/java/com/viandasApp/api/Emprendimiento/controller/EmprendimientoClienteController.java
@@ -51,9 +51,11 @@ public class EmprendimientoClienteController {
     })
     @GetMapping
     public ResponseEntity<PagedModel<EntityModel<EmprendimientoDTO>>> getAllEmprendimientos(
+            @RequestParam(required = false) String ciudad,
+            @RequestParam(required = false) String nombre,
             @PageableDefault(size = 10, page = 0) Pageable pageable
     ){
-        Page<Emprendimiento> page = emprendimientoService.buscarEmprendimientos(null, null, null, null, pageable);
+        Page<Emprendimiento> page = emprendimientoService.buscarEmprendimientos(null, ciudad, nombre, null, pageable);
 
         Page<EmprendimientoDTO> dtoPage = page.map(EmprendimientoDTO::new);
 
